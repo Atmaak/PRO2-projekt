98e2d02faeb7945de89c605b5d7274e89f81abf2
Atmaak <77574694+Atmaak@users.noreply.github.com>
2025-05-16 18:32:18 +0200

    x

---
+++ src/main/java/cz/uhk/pro2_a/controller/UserController.java
@@ -0,0 +1,64 @@
+package cz.uhk.pro2_a.controller;
+
+import cz.uhk.pro2_a.model.User;
+import cz.uhk.pro2_a.repository.UserRepository;
+import cz.uhk.pro2_a.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String list(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users_list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(Model model, @PathVariable long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "users_detail";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(Model model, @PathVariable long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "users_delete";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteConfirm(Model model, @PathVariable long id) {
+        userService.deleteUser(id);
+        return "redirect:/users/";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("user", new User());
+        return "users_add";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "users_add";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/users/";
+    }
+
+}
